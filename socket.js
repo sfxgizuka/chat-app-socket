67deea01e0157f53a7d0a202ce779df40559dde1
Gregory Izuka <gizuka@seamfix.com>
2024-01-14 00:10:27 +0100

    Added way to handle send message

--- socket.js
+++ socket.js
@@ -5,6 +5,47 @@ const io = require('socket.io')(8000,{
     }
 })
 
+let users = [];
+const addUser = (userId,socketId,userInfo) => {
+     const checkUser = users.some(u=> u.userId === userId );
+
+     if(!checkUser){
+          users.push({userId,socketId,userInfo});
+     }
+}
+
+const userRemove = (socketId) => {
+     users = users.filter(u=>u.socketId !== socketId );
+}
+
+const findFriend = (id) => {
+     return users.find(u=>u.userId === id);
+}
+
 io.on('connection',(socket)=>{
-    console.log('Socket is connecting...')
+     console.log('Socket is connecting...')
+     socket.on('addUser',(userId,userInfo)=>{
+          addUser(userId,socket.id,userInfo);
+          io.emit('getUser',users);
+     })
+     socket.on('sendMessage',(data)=>{
+          const user = findFriend(data.reseverId);
+          if(user !== undefined){
+               socket.to(user.socketId).emit('getMessage',{
+                    senderId : data.senderId,
+                    senderName :  data.senderName,
+                    reseverId :  data.reseverId,
+                    createAt : data.time,
+                    message : {
+                         text : data.message.text,
+                         image : data.message.image
+                    }
+               })
+          }
+     })
+     socket.on('disconnect',() =>{
+          console.log('user is disconnect... ');
+          userRemove(socket.id);
+          io.emit('getUser',users);
+     })
 })
